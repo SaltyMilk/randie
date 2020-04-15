f551ed572b6f34e342b8811ee44cd8de97cfcf1f
SaltyMilk <soufiane.elmelcaoui@gmail.com>
2020-04-15 02:16:32 +0200

    V0.2

--- randie.c
+++ randie.c
@@ -12,15 +12,14 @@ int check_id()
 }
 
 // Pool of commands
-char *base_cmd[CMD_COUNT] = {"gcc " , "echo ", "mkdir ", "touch ", "rm ", "cat "};
+char *base_cmd[CMD_COUNT] = {"gcc " , "echo ", "mkdir ", "touch ", "rm ", "echo "};
 
 char *add_arg(char *bcmd, int arg_len)
 {
 	int al = rand() % arg_len + 1;
 	char *fcmd;
 	
-	int t_size = strlen(bcmd) + al + 1;
-	printf("size=%d, arglen=%d\n", t_size, arg_len);
+	int t_size = strlen(bcmd) + al + 1 + 7;
 	if (!(fcmd = malloc(t_size)))
 		return (NULL);
 	int i = 0;
@@ -32,6 +31,7 @@ char *add_arg(char *bcmd, int arg_len)
 		fcmd[i++] = (rand() % 95) + 32;
 		// Only take chars between ascii 32(' ') and 126('~')
 	fcmd[i] = 0;
+	strcat(fcmd, " >> log");
 	return (fcmd);
 }
 
@@ -39,23 +39,25 @@ void random_cmd(int arg_len)
 {
 	time_t	t = time(0);
 	char	*bcmd;
-	srand(t*42);
+	srand(t);
 	bcmd = base_cmd[rand() % CMD_COUNT];
 	bcmd = add_arg(bcmd, arg_len);
 	system(bcmd);
 }
 
-#define DEF_ARG_LEN 10
+#define MAX_ARG_LEN 19
 
 int main(int argc, char **argv)
 {
 	int arg_len;
+	time_t	t = time(0);
 
 	if (check_id())
 		return (1);
+	srand(t * 19);
 	if (argc == 2 && !(arg_len = atoi(argv[1])) && printf("[Invalid argument]: %s num\n", argv[0]))
 		return (1);
 	else if (argc != 2)
-		arg_len = DEF_ARG_LEN;
-	random_cmd(arg_len);
+		arg_len = (rand() % MAX_ARG_LEN) + 1;
+		random_cmd(arg_len);
 }
---
+++ start_randie.sh
@@ -0,0 +1,3 @@
+#Use this to launch randie, note you'll need to run asroot
+gcc randie.c -o randie
+while true; do ./randie;done
